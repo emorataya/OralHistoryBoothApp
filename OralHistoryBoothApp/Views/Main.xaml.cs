a10113a09d545ca44b1e63e9d291c890d5d5db3f
emorataya <emorataya@harding.edu>
2022-12-09 19:07:08 -0600

    Added recording page

--- OralHistoryBoothApp/Views/Main.xaml.cs
+++ OralHistoryBoothApp/Views/Main.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using OralHistoryBoothApp.Views;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +26,13 @@ namespace OralHistoryBoothApp
         public MainPage()
         {
             this.InitializeComponent();
+        }
+        Recordingpage recordingpage;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Recordingpage));
+
         }
     }
 }
