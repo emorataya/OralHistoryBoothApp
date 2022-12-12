3451460fb0726b783ed821998cff10276b74d593
emorataya <emorataya@harding.edu>
2022-12-12 16:17:01 -0600

    Added Log In View

---
+++ OralHistoryBoothApp/LogInPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using OralHistoryBoothApp.View;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace OralHistoryBoothApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LogInPage : Page
+    {
+        public LogInPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void GuestBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(TermsPage));
+        }
+
+        private void AdminBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //private void GuestBtn(object sender, RoutedEventArgs e)
+        //{
+        //    this.Frame.Navigate(typeof(TermsPage));
+        //}
+    }
+}
